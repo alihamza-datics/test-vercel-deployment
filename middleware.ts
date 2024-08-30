c619e99ba2633de618b2d8029d9387a974f65022
Syed-AliHamza <hamza.syed9995@gmail.com>
2024-08-30 16:11:47 +0500

    public route and private route case handled

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,14 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server';
 
 const isProtectedRoute = createRouteMatcher(['/profile(.*)', '/companyProfile(.*)', '/chat(.*)', '/'])
+const isPublicRoute = createRouteMatcher(['/login', '/signup',])
 
 export default clerkMiddleware((auth, req) => {
+  const { userId } = auth();
+  if (userId && isPublicRoute(req)) {
+    return NextResponse.redirect(new URL(`${process.env.API_BASE_URL}/`, req.url));
+  }
   if (isProtectedRoute(req)) auth().protect({
     unauthenticatedUrl: `${process.env.API_BASE_URL}/login`
   })
